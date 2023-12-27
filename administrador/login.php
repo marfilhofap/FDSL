6c31469fe6235c30b57ade0feb304667588f6106
Marfilho Fidel Alvares Pereira <marfilhofap@gmail.com>
2023-12-27 14:13:40 +0900

    Update - Gitignore

--- administrador/login.php
+++ administrador/login.php
@@ -37,20 +37,22 @@ session_destroy();
 
     <style>
         .divider:after,
-.divider:before {
-content: "";
-flex: 1;
-height: 1px;
-background: #eee;
-}
-.h-custom {
-height: calc(100% - 73px);
-}
-@media (max-width: 450px) {
-.h-custom {
-height: 100%;
-}
-}
+        .divider:before {
+            content: "";
+            flex: 1;
+            height: 1px;
+            background: #eee;
+        }
+
+        .h-custom {
+            height: calc(100% - 73px);
+        }
+
+        @media (max-width: 450px) {
+            .h-custom {
+                height: 100%;
+            }
+        }
     </style>
 
 
@@ -67,34 +69,33 @@ height: 100%;
             <!-- Main Content -->
             <div id="content">
                 <div class="card">
-                    
+
                     <section class="vh-100">
                         <div class="container-fluid h-custom">
                             <div class="row d-flex justify-content-center align-items-center h-100">
-                            <div class="col-md-9 col-lg-6 col-xl-5">
-                                <img src="img/logo_fdsl.jpeg" class="img-fluid" alt="Sample image">
-                            </div>
-                            <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                                <form action="controller/j_utilijador/j_utilijador_handler.php" method="POST" class="col p-4 position-static" enctype="multipart/form-data">
-
-                                <div class="divider d-flex align-items-center my-4">
-                                    <h2 class="text-center fw-bold mx-3 mb-0">SISTEMA INFORMASAUN</h2>
+                                <div class="col-md-9 col-lg-6 col-xl-5">
+                                    <img src="img/logo_fdsl.jpeg" class="img-fluid" alt="Sample image">
                                 </div>
+                                <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                                    <form action="controller/j_utilijador/j_utilijador_handler.php" method="POST" class="col p-4 position-static" enctype="multipart/form-data">
 
-                                <!-- Email input -->
-                                <div class="form-outline mb-4">
-                                    <input type="text" id="id_rejistu" name="id_rejistu" class="form-control form-control-lg" placeholder="Hatama ita nia ID-Membro nian" />
-                                    <label class="form-label" for="id_rejistu">ID-Membro</label>
-                                </div>
+                                        <div class="divider d-flex align-items-center my-4">
+                                            <h2 class="text-center fw-bold mx-3 mb-0">LOGIN SISTEMA INFORMASAUN FDSL</h2>
+                                        </div>
 
-                                <!-- Password input -->
-                                <div class="form-outline mb-3">
-                                    <input type="password" id="password" name="password" class="form-control form-control-lg" placeholder="Hatama ita nia password" />
-                                    <label class="form-label" for="password">Password</label>
-                                </div>
+                                        <!-- Email input -->
+                                        <div class="form-outline mb-4">
+                                            <input type="text" id="id_rejistu" name="id_rejistu" class="form-control form-control-lg" placeholder="Hatama ita nia ID-Membro nian" />
+                                            <label class="form-label" for="id_rejistu">ID-Membro</label>
+                                        </div>
 
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <!-- Checkbox -->
+                                        <!-- Password input -->
+                                        <div class="form-outline mb-3">
+                                            <input type="password" id="password" name="password" class="form-control form-control-lg" placeholder="Hatama ita nia password" />
+                                            <label class="form-label" for="password">Password</label>
+                                        </div>
+
+                                        <!-- <div class="d-flex justify-content-between align-items-center">
                                     <div class="form-check mb-0">
                                     <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
                                     <label class="form-check-label" for="form2Example3">
@@ -102,45 +103,45 @@ height: 100%;
                                     </label>
                                     </div>
                                     <a href="#!" class="text-body">Haluha password?</a>
-                                </div>
+                                </div> -->
 
-                                <div class="text-center text-lg-start mt-4 pt-2">
-                                    <button type="submit" name="login_utilijador" class="btn btn-primary btn-lg" style="padding-left: 2.5rem; padding-right: 2.5rem;" >
-                                        Login
-                                    </button>
- 
-                                </div>
+                                        <div class="text-center text-lg-start mt-4 pt-2">
+                                            <button type="submit" name="login_utilijador" class="btn btn-primary btn-lg" style="padding-left: 2.5rem; padding-right: 2.5rem;">
+                                                Login
+                                            </button>
 
-                                </form>
-                            </div>
+                                        </div>
+
+                                    </form>
+                                </div>
                             </div>
                         </div>
                         <div class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5" style="background-color: #006600;">
                             <!-- Copyright -->
                             <div class="text-white mb-3 mb-md-0">
-                            Copyright © <?= date('Y')?>. All rights reserved.
+                                Copyright © <?= date('Y') ?>. All rights reserved.
                             </div>
                             <!-- Copyright -->
 
                             <!-- Right -->
                             <div>
-                            <a href="#!" class="text-white me-4">
-                                <i class="fab fa-facebook-f"></i>
-                            </a>
-                            <a href="#!" class="text-white me-4">
-                                <i class="fab fa-twitter"></i>
-                            </a>
-                            <a href="#!" class="text-white me-4">
-                                <i class="fab fa-google"></i>
-                            </a>
-                            <a href="#!" class="text-white">
-                                <i class="fab fa-linkedin-in"></i>
-                            </a>
+                                <a href="#!" class="text-white me-4">
+                                    <i class="fab fa-facebook-f"></i>
+                                </a>
+                                <a href="#!" class="text-white me-4">
+                                    <i class="fab fa-twitter"></i>
+                                </a>
+                                <a href="#!" class="text-white me-4">
+                                    <i class="fab fa-google"></i>
+                                </a>
+                                <a href="#!" class="text-white">
+                                    <i class="fab fa-linkedin-in"></i>
+                                </a>
                             </div>
                             <!-- Right -->
                         </div>
-                        </section>
-                        
+                    </section>
+
                 </div>
             </div>
         </div>
--- config/parametros_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-#Dados do servidor POSTGRESQL do SIGAP-FOUN
-define('PSQLHOST', 'localhost');
-define('PSQLUSER', 'postgres');
-define('PSQLPW', 'admin');
-define('PSQLDB', 'db_fdsl');
-define('PSQLPORT', '5432');
